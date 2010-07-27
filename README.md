72251585d2d600b17772da0b317feae29f4f1799
ujihisa <ujihisa@gmail.com>
2010-07-27 15:54:14 -0700

    supports Windows (cygwin) only on documentation and spec

--- README.md
+++ README.md
@@ -2,19 +2,23 @@
 
     $ gem install copier
 
-will be available soon later.
-
-## usage
+## Usage
 
     require 'copier'
     Copier('text')
 
 run the code and then you can paste `text` by Cmd-v on Mac OS X, Ctrl-v on Windows, and as well on other platforms.
 
+## Platform
+
+This library supports the following platforms. I'm always welcome patches for supporting other platforms, including spec.
+
+* Mac OS X
+* Windows (cygwin)
+
 ## development
 
 issues
 
-* no windows support yet
 * no GNOME support yet
 * no KDE support yet
--- spec/copier_spec.rb
+++ spec/copier_spec.rb
@@ -8,8 +8,19 @@ require_relative '../lib/copier'
 
 describe 'Copier()' do
   it 'copies text on Mac OS X' do
+    pending 'This platform is not Mac OS X, so you cannot run this case now' unless
+      /darwin/ =~ RUBY_PLATFORM
     a = rand.to_s
     Copier(a)
     `pbpaste`.chomp.should == a
+  end
+
+  it 'copies text on Windows (cygwin)' do
+    pending 'This platform is not Windows (cygwin), so you cannot run this case now' unless
+      /cygwin/ =~ RUBY_PLATFORM
+    a = rand.to_s
+    Copier(a)
+    #`pbpaste`.chomp.should == a
+    pending 'Please fill this case... I dont know how to paste text'
   end
 end
