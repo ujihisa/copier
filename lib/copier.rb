f0999e4c429b6b533f47b1d78036eddfc45ab146
ujihisa <ujihisa@gmail.com>
2010-07-27 16:23:44 -0700

    implemented cygwin support

--- lib/copier.rb
+++ lib/copier.rb
@@ -2,6 +2,8 @@ def Copier(text)
   case RUBY_PLATFORM
   when /darwin/
     IO.popen('pbcopy', 'w') {|io| io.write text }
+  when /cygwin/
+    File.open('/dev/clipboard', 'wb') {|io| io.write text.gsub("\x0A", "\n") }
   else
     raise RUBY_PLATFORM + " is not supported yet by Copier."
   end
