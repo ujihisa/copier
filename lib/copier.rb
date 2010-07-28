2944d0ef6bb4292d1e88bb1bc8ef155d91a5c633
Sora Harakami <sora134@gmail.com>
2010-07-29 07:54:04 +0900

    copy_method and config file ~/.copier

--- lib/copier.rb
+++ lib/copier.rb
@@ -1,8 +1,56 @@
 module Copier
   class NotSupported < StandardError; end
+
+  class Config < Hash
+    def method_missing(name,*args)
+      if /=$/ =~ name.to_s
+        self[name] = args[0]
+      else
+        self[name]
+      end
+    end
+  end
+
+  class << self
+    def load_config(f = '~/.copier')
+      fn = File.expand_path(f)
+      @default_config = Config.new({:copy => nil})
+      config = @default_config
+      eval(File.read(fn)) rescue nil
+      @config = config
+      @config_loaded = true
+    end
+
+    def method_missing(name, *args)
+      @config.__send__(name, *args)
+    end
+
+    def disable_config_file=(a)
+      @disable_config_file = a
+      if a
+        @config = @default_config
+        @config_loaded = false
+      end
+    end
+
+    def disable_config_file
+      @disable_config_file
+    end
+
+    def config_loaded
+      @config_loaded
+    end
+  end
 end
 
 def Copier(text)
+  Copier.load_config unless Copier.config_loaded && !Copier.disable_config_file
+
+  if Copier.copy_method && !Copier.disable_config
+    Copier.copy_method.call(text)
+    return
+  end
+
   case RUBY_PLATFORM
   when /darwin/
     IO.popen('pbcopy', 'w') {|io| io.write text }
--- spec/copier_spec.rb
+++ spec/copier_spec.rb
@@ -7,20 +7,39 @@ end
 require_relative '../lib/copier'
 
 describe 'Copier()' do
-  it 'copies text on Mac OS X' do
-    pending 'This platform is not Mac OS X, so you cannot run this case now' unless
-      /darwin/ =~ RUBY_PLATFORM
-    a = rand.to_s
-    Copier(a)
-    `pbpaste`.chomp.should == a
+  describe 'in default case' do
+    before do
+      Copier.disable_config_file = true
+    end
+
+    it 'copies text on Mac OS X' do
+      pending 'This platform is not Mac OS X, so you cannot run this case now' unless
+        /darwin/ =~ RUBY_PLATFORM
+      a = rand.to_s
+      Copier(a)
+      `pbpaste`.chomp.should == a
+    end
+
+    it 'copies text on Windows (cygwin)' do
+      pending 'This platform is not Windows (cygwin), so you cannot run this case now' unless
+        /cygwin/ =~ RUBY_PLATFORM
+      a = rand.to_s
+      Copier(a)
+      #`pbpaste`.chomp.should == a
+      pending 'Please fill this case... I dont know how to paste text'
+    end
   end
 
-  it 'copies text on Windows (cygwin)' do
-    pending 'This platform is not Windows (cygwin), so you cannot run this case now' unless
-      /cygwin/ =~ RUBY_PLATFORM
-    a = rand.to_s
-    Copier(a)
-    #`pbpaste`.chomp.should == a
-    pending 'Please fill this case... I dont know how to paste text'
+  describe 'with config' do
+    before do
+      Copier.disable_config_file = false
+    end
+
+    it '.copy_method can copy by block' do
+      Copier.copy_method = lambda do |a|
+        a.should == "Copier will be used over ssh"
+      end
+      Copier("Copier wille be used over ssh")
+    end
   end
 end
